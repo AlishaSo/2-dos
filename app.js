52fd214b3ab9319c5684533f0764c32e91c4d6b8
poovaic <poovai.c@gmail.com>
2022-11-21 11:38:32 -0500

    pc-reworked-app.js

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const logger = require('morgan');
 const routes = require('./routes');
-const { errorHandler } = require('./middleware/errorMiddleware');
 
 const app = express();
 
@@ -13,6 +12,5 @@ app.use(logger('dev'));
 
 app.use('/api', routes);
 
-app.use(errorHandler);
 
 module.exports = app;
