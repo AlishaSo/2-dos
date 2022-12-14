a2f3c413abe9a338f571ca001e52bed9a3dd659b
poovaic <poovai.c@gmail.com>
2022-12-14 10:45:06 -0500

    Add and Create: update method

--- backend/controller/index.js
+++ backend/controller/index.js
@@ -5,7 +5,7 @@ const Task = require("../taskModel.js");
 const createTask = async (req, res) =>{
     
     if(!req.body.description) {
-        res.status(400).send('Description not received. Please enter a description!');
+        return res.status(400).send('Description not received. Please enter a description!');
       }
     const newTask = new Task({
         title:req.body.title,
@@ -17,7 +17,7 @@ const createTask = async (req, res) =>{
 
     try{
         const savedTask = await newTask.save();
-        res.status(200).json(savedTask);
+        return res.status(200).json(savedTask);
     
     }catch(err){
         res.status(500).json(err)
@@ -40,8 +40,45 @@ catch(err){
 }
 }
 
+//UPDATE
+
+const updateTask = async(req,res)=>{
+    
+    try{
+    
+        if(!req.body.description) {
+        return res.status(400).send('Description not received. Please enter a description!');
+      }
+      //get the id of the task from URL
+      const { id } = req.params;
+
+      const taskToBeUpdated = await Task.findById(id); 
+      if(taskToBeUpdated){
+        Task.findByIdAndUpdate(id, req.body, { new: true }, (e, taskToBeUpdated) => {
+            if(e != null) {
+              console.log(e, 'error');
+              res.status(404).send(message);
+            }
+            else {
+                console.log(taskToBeUpdated);
+                res.json(taskToBeUpdated);
+              }
+      });
+
+
+    }}
+   
+    catch(err){
+        res.status(500).json(err);
+
+    }
+}
+
+
 module.exports = {
    createTask,
-   getAllTasks
+   getAllTasks,
+   updateTask,
+
     
 }
