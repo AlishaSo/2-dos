3a08508dfc546f28871bc57363260fb246ae4a1b
poovaic <poovai.c@gmail.com>
2022-12-27 18:27:59 -0500

    create:client/components and screens

---
+++ client/src/screens/LandingPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Footer from '../components/Footer'
+
+function LandingPage() {
+  return (
+    <div className="landing_page">
+    
+    <header className="header_landing_page">
+        <h1>2-DOs</h1>
+    </header>
+
+    <Footer/>
+    
+    </div>
+  )
+}
+
+export default LandingPage
